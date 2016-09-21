b0bf19987b4f6afa393cd35bec9e48c530ef3fec
luckymore <njuluckwang@gmail.com>
2016-09-21 23:04:55 +0800

    mergeSort and findMaxSubArray

---
+++ Divide-and-Conquer/findMaxSubArray.js
@@ -0,0 +1,51 @@
+function findMaxSubArray(array,low,high) {
+    if (low == high) {
+        return [low,high,array[0]];
+    } 
+    var middle = Math.floor((low + high) / 2.0)
+    var firstHalfMaxArray = findMaxSubArray(array,0,middle)
+    var secondHalfMaxArray = findMaxSubArray(array,middle+1,high)
+
+    var crossMaxArray = findMaxCrossSubArray(array,low,middle,high)
+
+    var max = Math.max(crossMaxArray[2],Math.max(firstHalfMaxArray[2],secondHalfMaxArray[2]))
+    if (max == crossMaxArray[2]) {
+        return crossMaxArray
+    } 
+    if (max == secondHalfMaxArray[2]) {
+        return secondHalfMaxArray
+    }
+    if (max == firstHalfMaxArray[2]) {
+        return firstHalfMaxArray
+    }
+}
+
+
+function findMaxCrossSubArray(array,low,middle,high) {
+    var leftMaxSum = Number.NEGATIVE_INFINITY;
+    var sum = 0;
+    var leftStart = middle;
+    for (var i = middle; i >= low ; i--) {
+        sum = sum + array[i];
+        if (sum > leftMaxSum) {
+            leftMaxSum = sum;
+            leftStart = i;
+        }
+    }
+
+    var rightMaxSum = Number.NEGATIVE_INFINITY;
+    sum = 0;
+    var rightEnd = middle;
+    for (var j = middle+1; j <=high; j++){
+        sum = sum + array[j];
+        if (sum > rightMaxSum) {
+            rightMaxSum = sum;
+            rightEnd = j;
+        }
+    }
+    return [leftStart,rightEnd, rightMaxSum + leftMaxSum];
+}
+
+
+array = [1,-1,2,3,6];
+console.log(findMaxSubArray(array,0,4));
---
+++ Divide-and-Conquer/mergeSort.js
@@ -0,0 +1,41 @@
+function mergeSort(array, start, end) {
+    if (start < end) {
+        var middle = Math.floor((start + end) / 2);
+        mergeSort(array, start, middle);
+        mergeSort(array, middle+1, end);
+        merge(array,start,middle,end);
+     }
+}
+
+function merge(array, start, middle, end) {
+    var firstArray = [];
+    var secondArray = [];
+    for (var i = start; i <= middle; i ++) {
+        firstArray.push(array[i])
+    }
+    for (var i = middle+1; i <= end; i++) {
+        secondArray.push(array[i])
+    }
+    var i = 0;
+    var j = 0;
+    for (var k = start; k <= end; k++) {
+        if (i == firstArray.length) {
+            array[k] = secondArray[j]
+            j++;
+        } else if (j == secondArray.length) {
+            array[k] = firstArray[i];
+            i++;
+        } else if (firstArray[i] <= secondArray[j]){
+            array[k] = firstArray[i];  
+            i++;
+        } else {
+            array[k] = secondArray[j];
+            j++;
+        }
+    }
+}
+
+
+var array = [22,33,1,5,66,2,1232,98,151];
+mergeSort(array,0,array.length-1);
+console.log(array);
