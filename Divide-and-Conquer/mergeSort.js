2b66676dae5d48061fb970fe6b86a8628bff9a2f
luckymore <njuluckwang@gmail.com>
2016-09-22 18:36:33 +0800

    refacter with incline function

--- Divide-and-Conquer/mergeSort.js
+++ Divide-and-Conquer/mergeSort.js
@@ -1,41 +1,43 @@
-function mergeSort(array, start, end) {
-    if (start < end) {
-        var middle = Math.floor((start + end) / 2);
-        mergeSort(array, start, middle);
-        mergeSort(array, middle+1, end);
-        merge(array,start,middle,end);
-     }
-}
 
-function merge(array, start, middle, end) {
-    var firstArray = [];
-    var secondArray = [];
-    for (var i = start; i <= middle; i ++) {
-        firstArray.push(array[i])
-    }
-    for (var i = middle+1; i <= end; i++) {
-        secondArray.push(array[i])
-    }
-    var i = 0;
-    var j = 0;
-    for (var k = start; k <= end; k++) {
-        if (i == firstArray.length) {
-            array[k] = secondArray[j]
-            j++;
-        } else if (j == secondArray.length) {
-            array[k] = firstArray[i];
-            i++;
-        } else if (firstArray[i] <= secondArray[j]){
-            array[k] = firstArray[i];  
-            i++;
-        } else {
-            array[k] = secondArray[j];
-            j++;
+function myMergeSort(array) {
+    return mergeSort(array, 0, array.length - 1)
+    function mergeSort(array, start, end) {
+        function merge(array, start, middle, end) {
+            var firstArray = [];
+            var secondArray = [];
+            for (var i = start; i <= middle; i ++) {
+                firstArray.push(array[i])
+            }
+            for (var i = middle+1; i <= end; i++) {
+                secondArray.push(array[i])
+            }
+            var i = 0;
+            var j = 0;
+            for (var k = start; k <= end; k++) {
+                if (i == firstArray.length) {
+                    array[k] = secondArray[j]
+                    j++;
+                } else if (j == secondArray.length) {
+                    array[k] = firstArray[i];
+                    i++;
+                } else if (firstArray[i] <= secondArray[j]){
+                    array[k] = firstArray[i];  
+                    i++;
+                } else {
+                    array[k] = secondArray[j];
+                    j++;
+                }
+            }
+        }
+        if (start < end) {
+            var middle = Math.floor((start + end) / 2);
+            mergeSort(array, start, middle);
+            mergeSort(array, middle+1, end);
+            merge(array,start,middle,end);
         }
     }
 }
 
-
 var array = [22,33,1,5,66,2,1232,98,151];
-mergeSort(array,0,array.length-1);
+myMergeSort(array);
 console.log(array);
